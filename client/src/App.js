3ae3ebb1b02f52714f0d8cf8b9008d18bb265d35
Ponnparamaguru <2014170@ksrct.net>
2024-08-24 07:53:54 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import AdminDashboard from './components/AdminDashboard';
 import EmployeeDashboard from './components/EmployeeDashboard';
-
 import AssignTaskPage from './components/AssignTask'; 
 import AssignedTasksPage from './components/AssignedTask';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -15,10 +15,46 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/employee" element={<EmployeeDashboard />} /> 
-        <Route path="/assign-task" element={<AssignTaskPage />} />
-        <Route path="/view-tasks" element={<AssignedTasksPage />} />
+        
+        <Route 
+          path="/admin" 
+          element={
+            <ProtectedRoute 
+              element={<AdminDashboard />} 
+              allowedRoles={['admin']} 
+            />
+          } 
+        />
+        
+        <Route 
+          path="/employee" 
+          element={
+            <ProtectedRoute 
+              element={<EmployeeDashboard />} 
+              allowedRoles={['employee']} 
+            />
+          } 
+        />
+        
+        <Route 
+          path="/assign-task" 
+          element={
+            <ProtectedRoute 
+              element={<AssignTaskPage />} 
+              allowedRoles={['admin']} 
+            />
+          } 
+        />
+        
+        <Route 
+          path="/view-tasks" 
+          element={
+            <ProtectedRoute 
+              element={<AssignedTasksPage />} 
+              allowedRoles={['admin', 'employee']} 
+            />
+          } 
+        />
       </Routes>
     </Router>
   );
